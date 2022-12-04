fa9e7cf8be9dbdef393953fe824cd6e193e7c4a9
Nasuf MUTLU <nasufmutlu@hotmail.com>
2022-12-04 18:38:07 +0300

    .

---
+++ Business/Abstract/IDepartmentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace Business.Abstract
+{
+    public interface IDepartmentService
+    {
+        List<Department> GetList();
+        void Add(Department department);
+    }
+}
---
+++ Business/Concrete/DepartmentManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities;
+
+namespace Business.Concrete
+{
+    public class DepartmentManager:IDepartmentService
+    {
+        private IDeparmentDal _deparmentDal;
+
+        public DepartmentManager(IDeparmentDal deparmentDal)
+        {
+            _deparmentDal = deparmentDal;
+        }
+
+        public List<Department> GetList()
+        {
+            return _deparmentDal.GetAll();
+        }
+
+        public void Add(Department department)
+        {
+            _deparmentDal.Add(department);
+        }
+    }
+}
